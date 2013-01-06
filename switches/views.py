60d7744a06a39705bcc81d514f94464b47651e79
aleosd <osdalex@gmail.com>
2013-01-06 19:09:08 +0600

    improvements in database access

--- switches/views.py
+++ switches/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django.shortcuts import render
-from switches.models import Switch
+from switches.models import Switch, Street, SwitchType
 from django.contrib.auth.decorators import login_required
 
 @login_required
@@ -11,5 +11,27 @@ def index(request, status=None):
         switch_list = Switch.objects.filter(sw_ping=None)
     if status == 'warnings':
         switch_list = Switch.objects.filter(sw_uptime__lt=86400)
-    return render(request, 'switches/index.html', 
-                  {'switch_list': switch_list, 'status': status})
+
+    street_list = Street.objects.all()
+    street_dic = {}
+    for street in street_list:
+        street_dic[street.id] = street.street
+
+    sw_type_list = SwitchType.objects.all()
+    sw_type_dic = {}
+    for sw_type in sw_type_list:
+        sw_type_dic[sw_type.id] = sw_type.sw_type
+
+    render_dict = {}
+    for switch in switch_list:
+        render_dict[switch.id] = {'sw_id': switch.sw_id,
+                                  'sw_street': street_dic[switch.sw_street_id],
+                                  'sw_build_num': switch.sw_build_num,
+                                  'ip_addr': switch.ip_addr,
+                                  'sw_ping': switch.sw_ping,
+                                  'sw_uptime': switch.sw_uptime_str,
+                                  'sw_type': sw_type_dic[switch.sw_type_id],
+        }
+    print(render_dict)
+    return render(request, 'index.html',
+                  {'switch_list': switch_list, 'status': status, 'render_dict': render_dict,})
