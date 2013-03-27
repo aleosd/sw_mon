450bd08cad936689e82556ee2aa26c87915c1f9c
root <root@mon.(none)>
2013-03-27 21:52:19 +0600

    fixes

--- mysite/settings.py
+++ mysite/settings.py
@@ -68,7 +68,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    # '/var/www/mon/templates/static',
+    '/var/www/sw_mon/templates/static',
 )
 
 # List of finder classes that know how to find static files in
@@ -109,7 +109,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/var/www/mon/templates',
+    '/var/www/sw_mon/templates',
 )
 
 INSTALLED_APPS = (
