8778490d8a864fed56b8355b5f05b25539ce4769
aleosd <osdalex@gmail.com>
2013-03-28 11:41:42 +0600

    updated twitter bootstrap to 2.3.1, changed first page design

--- templates/static/js/support-scripts.js
+++ templates/static/js/support-scripts.js
@@ -1,14 +1,35 @@
+// Search-filter script for table
 $(document).ready(function() {
-    
     // Key pressed in search field, filter table
     $('#search').on('keyup', function() {
         var query = $(this).val();
         $('#sw_table tbody tr').each(function(e) {
             if (!query || $(this).children('#sw_addr, #sw_ip, #sw_type').text().toLowerCase().indexOf(query) > -1)
                 $(this).show();
-
             else
                 $(this).hide();
         });
     });
 });
+
+// Warn-Err views builder for table 
+$(document).ready(function() {
+    $('#warn').click(function() {
+        $(this).addClass('active');
+        $('#all').removeClass('active');
+        $('#sw_table tbody tr').each(function(e) {
+            if ($(this).hasClass('warning'))
+                $(this).show();
+            else
+                $(this).hide();
+        });
+    });
+});
+
+// table-sorting script
+$(document).ready(function()
+    {
+        $("#sw_table").tablesorter({sortList: [[0,0]]});
+    }
+);
+
