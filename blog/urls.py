5df0d81f05d9b867f6ec6d5cee4441ad5792bc83
aleosd <osdalex@gmail.com>
2013-04-22 10:32:36 +0600

    working on blog, changed paginator style on event's page

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('blog.views',
     url(r'^$', 'entries_index'),
     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\d\w]+)/$',
         'entry_detail'),
+    url(r'^categories/(?P<slug>[-\w]+)/$', 'category_detail'),
 )
