c60a84875b2f5d866ac1fdeb9091897ea9069ca0
aleosd <osdalex@gmail.com>
2013-05-06 16:27:12 +0600

    changed db quering from main view

--- switches/views.py
+++ switches/views.py
@@ -10,25 +10,14 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 @login_required
 def index(request, status=None):
     if not status:
-        switch_list = Switch.objects.all()
+        switch_list = Switch.objects.select_related().filter(sw_enabled=True)
     if status == 'errors':
-        switch_list = Switch.objects.filter(sw_ping=None)
+        switch_list = Switch.objects.select_related().filter(sw_ping=None)
     if status == 'warnings':
-        switch_list = Switch.objects.filter(
+        switch_list = Switch.objects.select_related().filter(
             Q(sw_uptime__lt=86400) | Q(sw_uptime=None)
         )
 
-    street_list = Street.objects.all()
-    street_dic = {}
-    for street in street_list:
-        street_dic[street.id] = street.street
-
-    sw_type_list = SwitchType.objects.all()
-    sw_type_dic = {}
-    for sw_type in sw_type_list:
-        sw_type_dic[sw_type.id] = sw_type.sw_type
-
-    render_dict = {}
     bad_uptime = 0
     bad_ping = 0
     for switch in switch_list:
@@ -37,36 +26,27 @@ def index(request, status=None):
                 bad_uptime += 1
             if not switch.sw_ping:
                 bad_ping += 1
-            render_dict[switch.id] = {'id': switch.id,
-                                      'sw_id': switch.sw_id,
-                                      'sw_street': street_dic[switch.sw_street_id],
-                                      'sw_build_num': switch.sw_build_num,
-                                      'ip_addr': switch.ip_addr,
-                                      'sw_ping': switch.sw_ping,
-                                      'sw_uptime': switch.sw_uptime_str,
-                                      'sw_uptime_sec': switch.sw_uptime,
-                                      'sw_type': sw_type_dic[switch.sw_type_id],
-                                      'sw_comment': switch.sw_comment,
-            }
+
     try:
         del request.session['instance']
     except:
         pass
 
-    return render(request, 'index.html',
-                  {'status': status, 'render_dict': render_dict,
-                   'bad_uptime': bad_uptime, 'bad_ping': bad_ping})
+    return render(request, 'mon/index.html',
+                  {'status': status,
+                   'bad_uptime': bad_uptime, 'bad_ping': bad_ping,
+                   'switch_list': switch_list})
 
 @login_required
 def edit(request, id=None):
     if not id:
-        return render(request, 'edit.html', {'form': SwitchForm()})
+        return render(request, 'mon/edit.html', {'form': SwitchForm()})
     else:
         events = Event.objects.filter(ev_switch=id)[:10]
         switch = Switch.objects.get(id=id)
         form = SwitchForm(instance=switch)
         request.session['instance'] = switch
-        return render(request, 'edit.html', {'form': form, 'events': events})
+        return render(request, 'mon/edit.html', {'form': form, 'events': events})
 
 
 @login_required
@@ -84,7 +64,7 @@ def create_switch(request):
                 pass
             return HttpResponseRedirect('/')
         else:
-            return render(request, 'edit.html', {'form': form})
+            return render(request, 'mon/edit.html', {'form': form})
 
 @login_required
 def history(request, status=None):
@@ -108,7 +88,7 @@ def history(request, status=None):
     except EmptyPage:
         event_list = paginator(paginator.num_pages)
 
-    return render(request, 'history.html', {'event_list': event_list,
+    return render(request, 'mon/history.html', {'event_list': event_list,
                                             'status' : status,
                                             'events_per_day' : t})
 
