0481bf2a00822d7d83fde365e832b0e7af38bd30
aleosd <osdalex@gmail.com>
2013-06-14 15:50:33 +0600

    Multilanguage improvements

--- mysite/urls.py
+++ mysite/urls.py
@@ -10,8 +10,12 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^mysite/', include('mysite.foo.urls')),
 
+    url(r'^$', 'switches.views.home_view'),
+    url(r'^blog/', include('blog.urls')),
     url(r'^new/$', 'switches.views.edit'),
+    url(r'^mon/', include('switches.urls')),
     url(r'^create/', 'switches.views.create_switch'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/login/'}),
     url(r'^chaining/', include('smart_selects.urls')),
     url(r'^i18n/', include('django.conf.urls.i18n')),
@@ -24,8 +28,4 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += i18n_patterns('',
-    url(r'^$', 'switches.views.home_view'),
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
-    url(r'^blog/', include('blog.urls')),
-    url(r'^mon/', include('switches.urls')),
 )
--- settings/settings.py
+++ settings/settings.py
@@ -116,6 +116,17 @@ ROOT_URLCONF = 'mysite.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.core.context_processors.request",
+    "django.contrib.messages.context_processors.messages",
+)
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
