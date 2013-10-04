96f65337c34d901be55eba648da972a963b9d2c2
aleosd <osdalex@gmail.com>
2013-10-04 20:11:28 +0600

    helper file for timing measurements

---
+++ helper_progs/timer.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python3
+
+import time
+
+
+class Timer():
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+
+    def __enter__(self):
+        self.start = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.end = time.time()
+        self.secs = self.end - self.start
+        self.msecs = self.secs * 1000
+        if self.verbose:
+            print('Running time {} ms'.format(self.msecs))
