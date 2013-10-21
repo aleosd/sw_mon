f2b6bf5cdbb991a1690694645610864b602d15d5
aleosd <osdalex@gmail.com>
2013-10-21 14:18:51 +0600

    working traffic pages, only menu left

--- graphs/views.py
+++ graphs/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from graphs.models import Graph
-from django.db.models import Q
 from django.http import Http404
 
 
-def graph_by_query(request, query):
-    imgs = Graph.objects.filter(Q(isp=query) | Q(period=query))
+def graph_by_query(request, query, type_):
+    if type_ == 'isp':
+        imgs = Graph.objects.filter(isp=query)
+    elif type_ == 'period':
+        pd = {'daily': 'd',
+              'weekly': 'w',
+              'monthly': 'm',
+              'yearly': 'y'}
+        imgs = Graph.objects.filter(period=pd[query])
+    print(query)
     if len(imgs) == 0:
         raise Http404
     return render(request, 'traf/graph_list.html', {'imgs': imgs})
