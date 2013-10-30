452de861b5aaa46f89621206ca102df4703026f0
aleosd <osdalex@gmail.com>
2013-10-30 10:42:27 +0600

    added redirect from main traffic page

--- graphs/urls.py
+++ graphs/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
+from django.views.generic import RedirectView
 
 
-urlpatterns = patterns('graphs.views',
-    url(r'^$', 'graph_by_query', {'query': 'ttl', 'type_': 'isp'}),
-    url(r'^isp/(?P<query>\w+)', 'graph_by_query', {'type_': 'isp'}),
-    url(r'^period/(?P<query>\w+)', 'graph_by_query', {'type_': 'period'}),
+urlpatterns = patterns('',
+    url(r'^$', RedirectView.as_view(url='isp/ttl/')),
+    url(r'^isp/(?P<query>\w+)', 'graphs.views.graph_by_query', {'type_': 'isp'}),
+    url(r'^period/(?P<query>\w+)', 'graphs.views.graph_by_query', {'type_': 'period'}),
     )
