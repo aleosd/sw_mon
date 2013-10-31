b5b076d221c75ae59a9647bc51fe31e12c5b798a
aleosd <osdalex@gmail.com>
2013-10-31 16:51:34 +0600

    fixes

--- helper_progs/rrd_ping.py
+++ helper_progs/rrd_ping.py
@@ -33,6 +33,12 @@ def update():
     logging.info('Updating rrd database')
     gateway_ping = switch.Host('10.1.10.1').sys_ping(packet_count=8)
     yandex_ping = switch.Host('ya.ru').sys_ping(packet_count=8)
+    if gateway_ping[0] is None:
+        list(gateway_ping)
+        gateway_ping[0] = 0.0
+    if yandex_ping[0] is None:
+        list(yandex_ping)
+        yandex_ping[0] = 0.0
     try:
         rrdtool.update(FILE_PATH + FILE_NAME,
                        'N:{}:{}:{}'.format(gateway_ping[1], gateway_ping[0], yandex_ping[0]))
